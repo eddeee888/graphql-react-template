eeea24fce24cafe0c852e3b6e8b8502be6a01bc2
Eddy Nguyen <github@eddeee888.me>
2025-02-24 21:23:41 +1100

    Handle page UI states better (#25)

--- src/App/BooksRoute/BooksRoute.tsx
+++ src/App/BooksRoute/BooksRoute.tsx
@@ -21,86 +21,97 @@ export const BooksRoute: FC = () => {
 
   const refreshButton = <button onClick={() => refetch()}>Reload</button>;
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error || !data || data.books.__typename === "ResultError") {
-    return (
-      <>
-        {refreshButton}
-        <div>Error</div>
-      </>
-    );
-  }
-
-  if (data.books.result.length === 0) {
-    return (
-      <>
-        {refreshButton}
-
-        <div>
-          No books. Create book <Link to="/books/create">here</Link>
-        </div>
-      </>
-    );
-  }
-
   return (
     <>
       {refreshButton} |{" "}
       <button onClick={() => setShownForm("create-book")}>Create book</button>
       <br />
       <br />
-      <table border={1}>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.books.result.map((book) => {
-            return (
-              <tr key={book.id}>
-                <td>{book.id}</td>
-                <td>
-                  <Link to={`/books/${book.id}`}>{book.isbn}</Link>
-                </td>
-                <td>
-                  <button
-                    onClick={() =>
-                      setShownForm({ form: "update-book", bookId: book.id })
-                    }
-                  >
-                    Edit
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <br />
-      {Array.from(
-        { length: data.books.pagination.totalPageCount },
-        (_, index) => {
+      {(function renderBooksList() {
+        if (loading) {
+          return <div>Loading...</div>;
+        }
+
+        if (error || !data || data.books.__typename === "ResultError") {
           return (
-            <Fragment key={index}>
-              {" "}
-              {index + 1 === currentPage ? (
-                currentPage
-              ) : (
-                <button onClick={() => setCurrentPage(index + 1)}>
-                  {index + 1}
-                </button>
-              )}{" "}
-              |{" "}
-            </Fragment>
+            <>
+              {refreshButton}
+              <div>Error</div>
+            </>
           );
-        },
-      )}
+        }
+
+        if (data.books.result.length === 0) {
+          return (
+            <>
+              {refreshButton}
+
+              <div>
+                No books. Create book <Link to="/books/create">here</Link>
+              </div>
+            </>
+          );
+        }
+
+        return (
+          <>
+            <table border={1}>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.books.result.map((book) => {
+                  return (
+                    <tr key={book.id}>
+                      <td>{book.id}</td>
+                      <td>
+                        <Link to={`/books/${book.id}`}>{book.isbn}</Link>
+                      </td>
+                      <td>
+                        <button
+                          onClick={() =>
+                            setShownForm({
+                              form: "update-book",
+                              bookId: book.id,
+                            })
+                          }
+                        >
+                          Edit
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+
+            <br />
+
+            {Array.from(
+              { length: data.books.pagination.totalPageCount },
+              (_, index) => {
+                return (
+                  <Fragment key={index}>
+                    {" "}
+                    {index + 1 === currentPage ? (
+                      currentPage
+                    ) : (
+                      <button onClick={() => setCurrentPage(index + 1)}>
+                        {index + 1}
+                      </button>
+                    )}{" "}
+                    |{" "}
+                  </Fragment>
+                );
+              },
+            )}
+          </>
+        );
+      })()}
       {shownForm === "create-book" && (
         <>
           <hr />
