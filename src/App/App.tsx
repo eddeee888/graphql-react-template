39195f271aadfe1c774bf2d757edfdee1a0fbfe9
Eddy Nguyen <github@eddeee888.me>
2025-11-02 15:08:49 +1100

    Migrate to Apollo Client v4

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,17 @@
 import type { FC } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { Defer20220824Handler } from "@apollo/client/incremental";
+import { ApolloProvider } from "@apollo/client/react";
 import { AppLayout } from "./AppLayout";
 import { BooksRoute } from "./BooksRoute";
 import { BookRoute } from "./BookRoute";
 
 const apolloClient = new ApolloClient({
-  uri: "http://localhost:4488/graphql", // https://github.com/eddeee888/graphql-server-template
+  link: new HttpLink({
+    uri: "http://localhost:4488/graphql", // https://github.com/eddeee888/graphql-server-template
+  }),
+  incrementalHandler: new Defer20220824Handler(),
   cache: new InMemoryCache(),
 });
 
---
+++ src/apollo-client.d.ts
@@ -0,0 +1,4 @@
+import type { Defer20220824Handler } from "@apollo/client/incremental";
+declare module "@apollo/client" {
+  export interface TypeOverrides extends Defer20220824Handler.TypeOverrides {}
+}
